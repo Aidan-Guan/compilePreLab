5ae6a91d8b0939607fc4efebe2e26c21a1fa6952
Aidan-Guan <3119084027@qq.com>
2021-10-05 22:59:09 +0800

    lab0-debug-数字连接字母

--- src/Operations.java
+++ src/Operations.java
@@ -13,40 +13,42 @@ public class Operations {
      */
     public static void readLine(String line) {
         Vector<String> tokens = splitLine(line);
+
         String[] speStrs = new String[] {"if", "else", "while", "break", "continue", "return"};
         List<String> speList = Arrays.asList(speStrs);
 
         for (String token : tokens) {
 
             // 判断是否为无符号整数
-            if (isDigit(token)) { System.out.println("Number("+token+")"); continue; }
+            if (isDigit(token)) { System.out.println("Number("+token+")"); }
             // 判断是否为标识符
-            if (isIdentifier(token) && !speList.contains(token)) { System.out.println("Ident("+token+")"); continue; }
-
-            // 判断是否为其他的东西
-            switch (token) {
-                case "if" -> { System.out.println("If");  }
-                case "else" -> { System.out.println("Else");  }
-                case "while" -> { System.out.println("While");  }
-                case "break" -> { System.out.println("Break");  }
-                case "continue" -> { System.out.println("Continue"); }
-                case "return" -> { System.out.println("Return"); }
-                case "=" -> { System.out.println("Assign"); }
-                case ";" -> { System.out.println("Semicolon"); }
-                case "(" -> { System.out.println("LPar"); }
-                case ")" -> { System.out.println("RPar"); }
-                case "{" -> { System.out.println("LBrace"); }
-                case "}" -> { System.out.println("RBrace"); }
-                case "+" -> { System.out.println("Plus"); }
-                case "*" -> { System.out.println("Mult"); }
-                case "/" -> { System.out.println("Div"); }
-                case "<" -> { System.out.println("Lt"); }
-                case ">" -> { System.out.println("Gt"); }
-                case "==" -> { System.out.println("Eq"); }
+            else if (isIdentifier(token) && !speList.contains(token)) { System.out.println("Ident("+token+")"); }
+            else {
+                // 判断是否为其他的东西
+                switch (token) {
+                    case "if" -> { System.out.println("If"); }
+                    case "else" -> { System.out.println("Else"); }
+                    case "while" -> { System.out.println("While"); }
+                    case "break" -> { System.out.println("Break"); }
+                    case "continue" -> { System.out.println("Continue"); }
+                    case "return" -> { System.out.println("Return"); }
+                    case "=" -> { System.out.println("Assign"); }
+                    case ";" -> { System.out.println("Semicolon"); }
+                    case "(" -> { System.out.println("LPar"); }
+                    case ")" -> { System.out.println("RPar"); }
+                    case "{" -> { System.out.println("LBrace"); }
+                    case "}" -> { System.out.println("RBrace"); }
+                    case "+" -> { System.out.println("Plus"); }
+                    case "*" -> { System.out.println("Mult"); }
+                    case "/" -> { System.out.println("Div"); }
+                    case "<" -> { System.out.println("Lt"); }
+                    case ">" -> { System.out.println("Gt"); }
+                    case "==" -> { System.out.println("Eq"); }
 //                default -> {
 //                    System.out.println("Err");
 //                    System.exit(0);
 //                }
+                }
             }
         }
     }
@@ -100,7 +102,12 @@ public class Operations {
                     // 向token中添加这个特殊字符
                     tokens.add(String.valueOf(str.charAt(index)).trim());
                 }
-                else {
+                else { // 是数字or字母的组合
+                    // 出现了数字开头接字母
+                    if (tmp.length()>=1 && Character.isDigit(tmp.charAt(0)) && !Character.isDigit(str.charAt(index))) {
+                        tokens.add(tmp);
+                        tmp = "";
+                    }
                     tmp = tmp + str.charAt(index);
                 }
             }
