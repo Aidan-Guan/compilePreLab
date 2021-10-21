85fc2244baef6ccb4866a2c730ff6e61f7bef4d0
Aidan-Guan <3119084027@qq.com>
2021-10-21 19:56:20 +0800

    完成词法分析

--- src/Lab1_LexicalAnalysisForGA.java
+++ src/Lab1_LexicalAnalysisForGA.java
@@ -1,11 +1,86 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Vector;
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.util.*;
 
 public class Lab1_LexicalAnalysisForGA {
+
+    public static Map<String, String> tokenTable = new HashMap<>();
+
+
+    public static Lab1_Token getNextToken (PushbackReader in) throws IOException {
+        char tokenChar = (char) in.read();
+        String tokenStr = "";
+
+        // 读取空字符
+        while (Character.isWhitespace(tokenChar)) { tokenChar = (char) in.read(); }
+
+        // 首字符是字母
+        if (Character.isLetter(tokenChar)) {
+            // 识别标识符
+            while (Character.isLetterOrDigit(tokenChar)) {
+                tokenStr += tokenChar;
+                tokenChar = (char) in.read();
+            }
+            in.unread(tokenChar);
+
+            if (tokenTable.containsKey(tokenStr))
+                return new Lab1_Token("RESERVED", tokenTable.get(tokenStr));
+            else
+                return new Lab1_Token("IDENT", tokenStr);
+        }
+        else if (Character.isDigit(tokenChar)) {
+            if (tokenChar != '0') { // 一定是十进制 非 0
+                while (Character.isDigit(tokenChar)) {
+                    tokenStr += tokenChar;
+                    tokenChar = (char) in.read();
+                }
+                in.unread(tokenChar);
+                return new Lab1_Token("NUMBER", tokenStr);
+            }
+            else {
+                tokenChar = (char) in.read();
+                // 单纯为0的情况
+                if (!Character.isDigit(tokenChar) && tokenChar!='x' && tokenChar!='X') {
+                    in.unread(tokenChar);
+                    return new Lab1_Token("NUMBER", "0");
+                }
+                // 16进制
+                else if (tokenChar == 'x' || tokenChar == 'X') {
+                    tokenStr = "";
+                    tokenChar = (char)in.read();
+                    while (Character.isDigit(tokenChar)) {
+                        tokenStr += tokenChar;
+                        tokenChar = (char) in.read();
+                    }
+                    in.unread(tokenChar);
+                    return new Lab1_Token("NUMBER", String.valueOf(Integer.parseInt(tokenStr, 16)));
+                }
+                // 8进制
+                else {
+                    while (Character.isDigit(tokenChar)) {
+                        tokenStr += tokenChar;
+                        tokenChar = (char) in.read();
+                    }
+                    in.unread(tokenChar);
+                    return new Lab1_Token("NUMBER", String.valueOf(Integer.parseInt(tokenStr,8)));
+                }
+            }
+        }
+        else {
+            switch (tokenChar) {
+                case ';' -> { return new Lab1_Token("SIGN", ";"); }
+                case '(' -> { return new Lab1_Token("SIGN", "("); }
+                case ')' -> { return new Lab1_Token("SIGN", ")"); }
+                case '{' -> { return new Lab1_Token("SIGN", "{"); }
+                case '}' -> { return new Lab1_Token("SIGN", "}"); }
+                default -> { System.exit(1); }
+            }
+        }
+
+        return null;
+    }
 
 
 
