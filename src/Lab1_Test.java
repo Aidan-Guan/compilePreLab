d822761b61038fdd526b879c151d0bd528f257ac
Aidan-Guan <3119084027@qq.com>
2021-10-23 19:37:35 +0800

    更改输入方式

--- src/Lab1_Test.java
+++ src/Lab1_Test.java
@@ -6,19 +6,19 @@ public class Lab1_Test {
     public static String outputStr = "";
 
     public static void main(String[] args) throws IOException {
-//        String fileLoc = args[0];
-        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
+        String fileLoc = args[0];
+//        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
         in = new PushbackReader(new FileReader(fileLoc));
         Lab1_LexicalAnalysisForGA.getTokenInit(in);
 
         Lab1_GrammarAnalysis.compUnitAnal();
 
 
-//        String outputFileLoc = args[1];
-//        FileWriter fileWriter = new FileWriter(outputFileLoc, true);
-//        fileWriter.write(outputStr.trim());
-//        fileWriter.flush();
-//        fileWriter.close();
+        String outputFileLoc = args[1];
+        FileWriter fileWriter = new FileWriter(outputFileLoc, true);
+        fileWriter.write(outputStr.trim());
+        fileWriter.flush();
+        fileWriter.close();
 
         System.out.print(outputStr.trim());
     }
