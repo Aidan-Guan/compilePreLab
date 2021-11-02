878e350d661dcfbff81cdd8834e0155420c41b1d
Aidan-Guan <3119084027@qq.com>
2021-11-03 01:17:53 +0800

    test初步架构

--- src/Lab3_Test.java
+++ src/Lab3_Test.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Vector;
 
 /**
  * https://github.com/Aidan-Guan/compilePreLab.git
@@ -9,8 +10,14 @@ import java.io.*;
 public class Lab3_Test {
     public static PushbackReader in;
     public static String outputStr = "";
+    public static Vector<Lab3_Token> allTokens = new Vector<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+//        String fileLoc = args[0];
+        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
+
+        in = new PushbackReader(new FileReader(fileLoc));
+
 
     }
 }
