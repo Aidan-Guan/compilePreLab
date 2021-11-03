f0333e577ed4d5b47af7870fa9e663a9788c2e6c
Aidan-Guan <3119084027@qq.com>
2021-11-03 23:12:01 +0800

    debug

--- src/Lab2_Test.java
+++ src/Lab2_Test.java
@@ -12,8 +12,8 @@ public class Lab2_Test {
     public static String outputStr = "";
 
     public static void main(String[] args) throws IOException {
-//        String fileLoc = args[0];
-        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
+        String fileLoc = args[0];
+//        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
 
         in = new PushbackReader(new FileReader(fileLoc));
         Lab2_LexicalAnalysisForGA.getTokenInit(in);
@@ -21,11 +21,11 @@ public class Lab2_Test {
 
         Lab2_GrammarAnalysis.compUnitAnal();
 
-//        String outputFileLoc = args[1];
-//        FileWriter fileWriter = new FileWriter(outputFileLoc, true);
-//        fileWriter.write(outputStr.trim());
-//        fileWriter.flush();
-//        fileWriter.close();
+        String outputFileLoc = args[1];
+        FileWriter fileWriter = new FileWriter(outputFileLoc, true);
+        fileWriter.write(outputStr.trim());
+        fileWriter.flush();
+        fileWriter.close();
 
         System.out.print(outputStr.trim());
     }
