4a1f2395b47e6129db2c5600571285b2d916efb5
Aidan-Guan <3119084027@qq.com>
2021-11-07 17:51:15 +0800

    重构

--- src/Test.java
+++ src/Test.java
@@ -12,22 +12,31 @@ public class Test {
     public static String outputStr = "";
 
     public static void main(String[] args) throws IOException {
-        String fileLoc = args[0];
-//        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
+//        String fileLoc = args[0];
+        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
 
         in = new PushbackReader(new FileReader(fileLoc));
         LexAnal.getTokenInit(in);
         LexAnal.myLex(fileLoc);
 
-        GrammarAnal.compUnitAnal();
+//        GrammarAnal.compUnitAnal();
+        GrammarAnalysis grammarAnalysis = init(in);
+        grammarAnalysis.CompUnit();
+        outputStr = grammarAnalysis.out;
 
-        String outputFileLoc = args[1];
-        FileWriter fileWriter = new FileWriter(outputFileLoc, true);
-        fileWriter.write(outputStr.trim());
-        fileWriter.flush();
-        fileWriter.close();
+
+//        String outputFileLoc = args[1];
+//        FileWriter fileWriter = new FileWriter(outputFileLoc, true);
+//        fileWriter.write(outputStr.trim());
+//        fileWriter.flush();
+//        fileWriter.close();
 
         System.out.print(outputStr.trim());
+    }
+
+    private static GrammarAnalysis init(PushbackReader in) {
+        GrammarAnalysis grammarAnalysis = new GrammarAnalysis(in);
+        return grammarAnalysis;
     }
 }
 
