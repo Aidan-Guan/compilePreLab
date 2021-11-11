69c8c6fdfc8377b6bb07008b66a46dc59ad19bed
Aidan-Guan <3119084027@qq.com>
2021-11-11 16:45:36 +0800

    token 类的重构

---
+++ src/Token/Token.java
@@ -0,0 +1,11 @@
+package Token;
+
+public class Token {
+    String value = "";
+    String type = "";
+
+    public Token(String value, String type) {
+        this.type = type;
+        this.value = value;
+    }
+}
