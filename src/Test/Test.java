5f2b9ec9e6bf1e6ec564b12b64d030e68276f2f8
Aidan-Guan <3119084027@qq.com>
2021-11-11 16:42:34 +0800

    test 重构完成

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,8 +2,35 @@ package Test;
 
 import java.io.*;
 
-public class Test {
-    public static void main(String[] args) {
+/**
+ * https://github.com/Aidan-Guan/compilePreLab.git
+ * Aidan-Guan
+ * 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
+ */
 
+public class Test {
+
+    public static PushbackReader in;
+    public static String outputStr = "";
+
+    public static void main(String[] args) throws IOException {
+        String fileLoc = args[0];
+//        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
+
+        in = new PushbackReader(new FileReader(fileLoc));
+
+
+        filePrint(args[1]);
+    }
+
+    private static void filePrint(String fileLoc) throws IOException {
+        String outputFileLoc = fileLoc;
+        FileWriter fileWriter = new FileWriter(outputFileLoc, true);
+        fileWriter.write(outputStr.trim());
+        fileWriter.flush();
+        fileWriter.close();
+
+        System.out.print(outputStr.trim());
     }
 }
+
