6f22f8fd58a7995c607fe2744304b4063a1e7014
Aidan-Guan <3119084027@qq.com>
2021-11-23 08:37:34 +0800

    expvalue

---
+++ src/Expression/ExpValue.java
@@ -0,0 +1,11 @@
+package Expression;
+
+public class ExpValue {
+    int register;
+    String valueType;
+
+    public ExpValue(int register, String valueType) {
+        this.register = register;
+        this.valueType = valueType;
+    }
+}
