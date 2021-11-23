ba108424d6189ae4338a672fbe4f80fe935fa068
Aidan-Guan <3119084027@qq.com>
2021-11-23 08:17:01 +0800

    Test

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM openjdk:15-alpine
 WORKDIR /app/
 COPY ./src/* /app/
 RUN javac Operations.java
-RUN javac Test.java
+RUN javac TestCompile.Test.java
 
---
+++ src/TestCompile/Test.java
@@ -0,0 +1,16 @@
+package TestCompile;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+//ACCESS TOKEN  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
+
+public class Test {
+
+    public static Scanner in;
+
+    public static void main (String[] args) throws FileNotFoundException {
+
+    }
+}
