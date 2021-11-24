c2a24afd903aad6b099a720dc919349bb78d160b
Aidan-Guan <3119084027@qq.com>
2021-11-24 15:23:10 +0800

    map methods

---
+++ src/GenerateCode/GrammarCode/ASTToCode.java
@@ -0,0 +1,16 @@
+package GenerateCode.GrammarCode;
+
+import AST.*;
+
+public class ASTToCode {
+    public static AstNode root;
+
+    public static void generateIntermediateCode (AstNode ASTroot) {
+        root = ASTroot;
+
+    }
+
+    private static void identMapInit () {
+
+    }
+}
--- src/TokenUtils/IdentMapList.java
+++ src/TokenUtils/IdentMapList.java
@@ -1,6 +1,7 @@
 package TokenUtils;
 
 import ErrorSolution.ErrorSolu;
+import LexicalAnalysis.Token;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +17,36 @@ public class IdentMapList {
         return identMapList.get(mapListIndex-1);
     }
 
+    public static HashMap<String, Ident> getGlobalMap() {
+        if (mapListIndex == 0) ErrorSolu.error();
+
+        return identMapList.get(0);
+    }
+
+    public static void addMap(HashMap<String, Ident> newMap) {
+        identMapList.add(newMap);
+        mapListIndex++;
+    }
+
+    public static void removeFisrtMap() {
+        mapListIndex--;
+        identMapList.remove(mapListIndex);
+    }
+
+    public Ident getIdentInCurrMap(String str) {
+        HashMap<String, Ident> currMap = identMapList.get(mapListIndex-1);
+        return currMap.get(str);
+    }
+
+    public Ident getIdentInAllMap(String str) {
+        for (int i=mapListIndex-1; i>=0; i--) {
+            Ident tarIdent = identMapList.get(i).get(str);
+            if (tarIdent!=null) {
+                return tarIdent;
+            }
+        }
+        return null;
+    }
 
 
 }
