d7c7ff6b28b8e3504f117af86e329e1903515a36
Aidan-Guan <3119084027@qq.com>
2021-11-24 14:06:05 +0800

    stmtt

--- src/GrammarAnal/Grammar/Statement.java
+++ src/GrammarAnal/Grammar/Statement.java
@@ -1,12 +1,54 @@
 package GrammarAnal.Grammar;
 
 import AST.AstNode;
+import ErrorSolution.ErrorSolu;
+import LexicalAnalysis.Token;
 
 import java.util.ArrayList;
 
 import static GrammarAnal.Expression.Expressions.*;
 import static GrammarAnal.Grammar.GrammarAnalysis.*;
-import static GrammarAnal.Grammar.IfAnal.*;
+import static GrammarAnal.Grammar.TokensToAST.*;
 
 public class Statement {
+    public static void Stmt (AstNode parent) {
+        AstNode NodeStmt = new AstNode("<Stmt>");
+
+        if (currentSym.value.equals("return")) {
+            addChild(currentSym, NodeStmt);
+            getNextSym();
+            Exp(NodeStmt);
+            if (!currentSym.value.equals(";")) ErrorSolu.error();
+            addChild(currentSym, NodeStmt);
+            getNextSym();
+        }
+        else if (currentSym.value.equals("if")) {
+
+        }
+        else if (currentSym.value.equals("{")) {
+            Block(NodeStmt);
+        }
+        else if (currentSym.type.equals("IDENT")) {
+            Token tmp = showFutureSym(1);
+            if (tmp.value.equals("=")) {
+                getNextSym();
+                getNextSym();
+                Exp(NodeStmt);
+                if (!currentSym.value.equals(";")) ErrorSolu.error();
+                addChild(currentSym, NodeStmt);
+                getNextSym();
+            }
+            else {
+                Exp(NodeStmt);
+                if (!currentSym.value.equals(";")) ErrorSolu.error();
+                addChild(currentSym, NodeStmt);
+                getNextSym();
+            }
+        }
+        else {
+            ErrorSolu.error();
+        }
+
+        addChild(NodeStmt, parent);
+    }
 }
