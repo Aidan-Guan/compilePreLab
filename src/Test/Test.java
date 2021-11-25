d9bb735215d98135c5787eb0ff6187fdee7729ff
Aidan-Guan <3119084027@qq.com>
2021-11-26 03:51:33 +0800

    debug

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -25,8 +25,8 @@ public class Test {
 
 
     public static void main(String[] args) throws IOException {
-//        String fileLoc = args[0];
-        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
+        String fileLoc = args[0];
+//        String fileLoc = "/Users/guanhuaimin/本地磁盘/学习/本地_大三上/编译原理/labs/compilePreLab/src/a.txt";
 
         in = new PushbackReader(new FileReader(fileLoc));
 
